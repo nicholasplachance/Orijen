d9bd3911b3950cc1102dc6f985064dbcc383c8b7
nick lachance <nicholasplachance@gmail.com>
2019-12-10 16:27:01 -0600

    Add logo image and img directory

--- root/src/components/header/header.component.jsx
+++ root/src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './header.style.css';
+import logo from '../../img/orijen_logo.png';
 
 export const Header = props => {
 	return (
 		<header>
-			<h1>Orijen</h1>
+			<img src={logo} alt="" />
 			<h3>Nexus for Junior Developers</h3>
 		</header>
 	);
